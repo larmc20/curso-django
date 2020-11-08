653bca8ac845fcf1240483876d7d7f630d138576
Luiz Arthur Carvalho <larmc20@yahoo.com.br>
2020-11-08 11:25:44 -0300

    Configuraçoes Bootstrap ok

---
+++ pypro/base/django_assertions.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+_test_case=TestCase()
+
+assert_contais=_test_case.assertContains
--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -3,3 +3,7 @@ from django.test import Client
 def test_status_code(client:Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+def test_status_code(client:Client):
+    resp = client.get('/')
+    assert resp.status_code == 200
