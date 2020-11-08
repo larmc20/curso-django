371eb4bec4a3100c96077c6c439d2156e7f48c6b
Luiz Arthur Carvalho <larmc20@yahoo.com.br>
2020-11-08 11:39:54 -0300

    Test cases installed

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,9 +1,10 @@
 from django.test import Client
+from pypro.django_assertions import assert_contains
 
 def test_status_code(client:Client):
     resp = client.get('/')
     assert resp.status_code == 200
 
-def test_status_code(client:Client):
+def test_title(client:Client):
     resp = client.get('/')
-    assert resp.status_code == 200
+    assert_contains(resp, '<title>Calc Pontos</title>')
